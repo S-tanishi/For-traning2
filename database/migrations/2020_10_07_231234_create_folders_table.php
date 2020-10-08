4021cb3fa7897379a98d6deb80ae5a2483b8c921
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-08 22:38:14 +0900

    add:フォルダ一覧表示機能

--- database/migrations/2020_10_07_231234_create_folders_table.php
+++ database/migrations/2020_10_07_231234_create_folders_table.php
@@ -14,7 +14,7 @@ class CreateFoldersTable extends Migration
     public function up()
     {
         Schema::create('folders', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->timestamps();
         });
     }
