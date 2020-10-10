d9280a044fddb19441bc3c0c0584d7cd4564d9e0
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-10 20:49:31 +0900

    add:フォルダ一覧機能

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,4 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width"=device-width, initial-scale=1.0">
-  <meta </head></html>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ToDo App</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<header>
+  <nav class="my-navbar">
+    <a class="my-navbar-brand" href="/">ToDo App</a>
+  </nav>
+</header>
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-4"></div></div></div></main></body></html>
