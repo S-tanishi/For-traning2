5c6bf73d4d03b598171eae9a570add947fbe8d60
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-10 20:59:14 +0900

    add:フォルダ一覧表示　テンプレート

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,4 +16,28 @@
 <main>
   <div class="container">
     <div class="row">
-      <div class="col col-md-4"></div></div></div></main></body></html>
+      <div class="col col-md-4">
+        <nav class="panel panel-default">
+          <div class="panel panel-=heading">フォルダ</div>
+          <div class="panel-body">
+            <a href="/" class="btn btn-default btn-block">
+              フォルダを追加する
+            </a>
+          </div>
+          <div class="list group">
+            @foreach($folders as $folder)
+              <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item">
+                {{ $folder->title }}
+              </a>
+            @endforeach
+          </div>
+        </nav>
+      </div>
+    <div class="column col-md-8">
+        <!-- タスクがここに表示 -->
+    </div>
+  </div>
+</div>
+</main>
+</body>
+</html>
