7c42129aeefe688c9b178d18e8a14b67cc0b7f37
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-14 21:15:53 +0900

    add:フォルダ一覧表示機能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,6 +12,10 @@ class TaskController extends Controller
     {
         $folders = Folder::all();
 
+        $current_folder = Folder::find($id);
+
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
