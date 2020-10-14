e0644cf1cef5f385aec3a0c560e93047d8a11170
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-15 08:27:16 +0900

    add:フォルダ一覧表示機能

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,5 +9,20 @@ class Task extends Model
 {
     use HasFactory;
 
-    const STATUS
+    const STATUS = [
+        1 => [ 'label' => '未着手' ],
+        2 => [ 'label' => '着手中' ],
+        3 => [ 'label' => '完了' ],   
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
 }
