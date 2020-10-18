e08f28ab0eb12332d9919a4df41114d1aedb4d2d
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-10-18 20:50:52 +0900

    add:フォルダ作成機能

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -10,4 +10,18 @@ class FolderController extends Controller
     {
         return view('folders/create');
     }
+
+    public function create(Request $request)
+{
+    // フォルダモデルのインスタンスを作成する
+    $folder = new Folder();
+    // タイトルに入力値を代入する
+    $folder->title = $request->title;
+    // インスタンスの状態をデータベースに書き込む
+    $folder->save();
+
+    return redirect()->route('tasks.index', [
+        'id' => $folder->id,
+    ]);
+}
 }
